c97864f757cd2f097c0eb004ef6c0c99131892b3
80andAbove <shogomiyagi94@gmail.com>
2021-12-16 18:19:03 -0800

    Added styling

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Testing push'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('My first app')),
+        body: Text('This is my default text'),
+      ),
     );
   }
 }
