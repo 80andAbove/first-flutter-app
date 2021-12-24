c07dfbe4a0ef92e81c1c3d5868b497afebb0a598
80andAbove <shogomiyagi94@gmail.com>
2021-12-24 15:15:59 +0900

    Implemented question index

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Question answered!');
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite movie?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(title: Text('My first app')),
           body: Column(
             children: [
-              Text('The question is here!'),
+              Text(questions[questionIndex]),
               RaisedButton(
-                child: Text('Answer 1'),
+                child: Text(questions[questionIndex]),
                 onPressed: answerQuestion,
               ),
               RaisedButton(
